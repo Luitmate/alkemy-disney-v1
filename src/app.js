2ace8512a217746d8f0253c4be493f02f0c9b638
lunov08 <lukitmate@gmail.com>
2022-01-20 04:12:28 -0300

    config sequelize cli

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const res = require('express/lib/response')
+const sequelize = require('sequelize')
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -10,6 +12,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
   console.log(`Example app listening at http://localhost:${PORT}`)
+  try {
+    await sequelize.authenticate()
+    console.log("Se estableció la conexión on la base de datos")
+  } catch (error) {
+      console.log(error)  
+  }
 })
