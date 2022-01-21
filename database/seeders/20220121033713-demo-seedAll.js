a0322fcdb0be4e9f974b43d4b40e6bc5ce7a3b41
lunov08 <lukitmate@gmail.com>
2022-01-21 00:48:36 -0300

    refactor seeds

---
+++ database/seeders/20220121033713-demo-seedAll.js
@@ -0,0 +1,114 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    let genreArray = [{
+      id: 11,
+      name: "Action",
+      image: "https://robohash.org/coneturporrorerum.jpg?size=400x300&set=set1",
+    }, {
+      id: 12,
+      name: "Comedy",
+      image: "https://robohash.org/courporrorerum.jpg?size=400x300&set=set1",  
+    }, {
+      id: 13,
+      name: "Drama",
+      image: "https://robohash.org/consecrporrorerum.jpg?size=400x300&set=set1",
+    }, {
+      id: 14,
+      name: "Horror",
+      image: "https://robohash.org/consectererum.jpg?size=400x300&set=set1",
+    }]
+
+    let movieArray = [{
+      id: 101,
+      image: "https://robohash.org/ullamrecusandaeexcepturi.jpg?size=400x300&set=set1",
+      title: 'The Avengers',
+      releaseYear: 2012,
+      ratingIMDB: 3,
+      GenreId: 11
+    }, {
+      id: 102,
+      image: "https://robohash.org/ullamrecudfdandaeexcepturi.jpg?size=400x300&set=set1",
+      title: 'Iron Man 3',
+      releaseYear: 2013,
+      ratingIMDB: 5,
+      GenreId: 12
+    }, {
+      id: 103,
+      image: "https://robohash.org/ullamrecusandaeesdfxcepturi.jpg?size=400x300&set=set1",
+      title: 'Wonder Woman',
+      releaseYear: 2017,
+      ratingIMDB: 3,
+      GenreId: 13
+
+    }, {
+      id: 104,
+      image: "https://robohash.org/ullamrecsdsafeusandaeexcepturi.jpg?size=400x300&set=set1",
+      title: 'Black Widow',
+      releaseYear: 2021,
+      ratingIMDB: 4,
+      GenreId: 11
+    }]
+
+    let characterArray = [{
+      id: 1,
+      image: "https://robohash.org/consecteturporrorerum.jpg?size=400x300&set=set1",
+      name: "Robert Downey Jr.",
+      age: 56,
+      weight: 80,
+      story: "In congue. Etiam justo. Etiam pretium iaculis justo. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum.",
+    }, {
+      id: 2,
+      image: "https://robohash.org/earumquoaperiam.jpg?size=400x300&set=set1",
+      name: "Chris Evans",
+      age: 40,
+      weight: 70,
+      story: "Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula",
+    }, {
+      id: 3,
+      image: "https://robohash.org/officiispossimusipsam.jpg?size=400x300&set=set1",
+      name: "Scarlett Johansson",
+      age: 37,
+      weight: 68,
+      story: "Donec ut dolor utpat, quam pedse lobortis ligula",
+    }, {
+      id: 4,
+      image: "https://robohash.org/beataearchitectoperspiciatis.jpg?size=400x300&set=set1",
+      name: "Gal Gadot",
+      age: 36,
+      weight: 69,
+      story: "Donec ut dolor utpat, quam pede lobortis ligula",
+    }]
+
+    let associationsArray = [{
+      CharacterId: 1,
+      MovieId: 101
+    } , {
+      CharacterId: 1,
+      MovieId: 102
+    }, {
+      CharacterId: 2,
+      MovieId: 101
+    }, {
+      CharacterId: 3,
+      MovieId: 104
+    }, {
+      CharacterId: 4,
+      MovieId: 103
+    }] 
+
+     await queryInterface.bulkInsert('genres', genreArray, {});
+     await queryInterface.bulkInsert('movies', movieArray, {});
+     await queryInterface.bulkInsert('characters', characterArray, {});
+     await queryInterface.bulkInsert('characterMovies', associationsArray, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('genres', null, {});
+    await queryInterface.bulkDelete('movies', null, {});
+    await queryInterface.bulkDelete('characters', null, {});
+    await queryInterface.bulkDelete('characterMovies', null, {});
+  }
+};
